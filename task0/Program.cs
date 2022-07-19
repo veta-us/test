fd5c1bf5e959c7652aebb62d2165038e08e0e84f
lisa <lisma_li@mail.ru>
2022-07-19 22:04:25 +0300

    сделала три задачи

---
+++ task0/Program.cs
@@ -0,0 +1,11 @@
+﻿string? inputLine = Console.ReadLine();
+if(inputLine != null)
+
+{
+    int inputNumber = int.Parse(inputLine);
+
+    int outNumber = inputNumber*inputNumber;
+
+    Console.WriteLine(outNumber);
+
+}
---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿string? inputLkneOne = Console.ReadLine();
+string? inputLkneTwo = Console.ReadLine();
+
+if(inputLkneOne != null && inputLkneTwo != null){
+    int inputNumberOne = int.Parse(inputLkneOne);
+    int inputNumberTwo = int.Parse(inputLkneTwo);
+
+    if(inputNumberOne == inputNumberTwo * inputNumberTwo)
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+
+if(inputNumberTwo == Math.Sqrt(inputNumberOne))
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+}
+

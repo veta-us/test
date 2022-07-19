6cb085779a6f82de6341f7b340beb6563f04f8b1
lisa <lisma_li@mail.ru>
2022-07-19 22:15:47 +0300

    второй вариант решение задачи с днями недели

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,16 +4,47 @@ if(inputLine != null)
 {
     int inputNumber = int.Parse(inputLine);
 
-    string[] dayOfWeek = new string[7];
-    dayOfWeek[0] = "Понедельник";
-    dayOfWeek[1] = "Вторник";
-    dayOfWeek[2] = "Среда";
-    dayOfWeek[3] = "Четверг";
-    dayOfWeek[4] = "Пятница";
-    dayOfWeek[5] = "Суббота";
-    dayOfWeek[6] = "Воскресенье";
+switch (inputNumber)
+{
+    case 1:
+    Console.WriteLine("Понедельник");
+    break;
+
+    case 2:
+    Console.WriteLine("Вторник");
+    break;
+
+    case 3:
+    Console.WriteLine("Среда");
+    break;
+
+    case 4:
+    Console.WriteLine("Четверг");
+    break;
+
+    case 5:
+    Console.WriteLine("Пятница");
+    break;
+
+    case 6:
+    Console.WriteLine("Суббота");
+    break;
+
+    case 7:
+    Console.WriteLine("Понедельник");
+    break;
+
+}
+//   string[] dayOfWeek = new string[7];
+//   dayOfWeek[0] = "Понедельник";
+//   dayOfWeek[1] = "Вторник";
+//   dayOfWeek[2] = "Среда";
+//   dayOfWeek[3] = "Четверг";
+//    dayOfWeek[4] = "Пятница";
+//    dayOfWeek[5] = "Суббота";
+//    dayOfWeek[6] = "Воскресенье";
 
 
-    Console.WriteLine(dayOfWeek[inputNumber-1]);
+ //   Console.WriteLine(dayOfWeek[inputNumber-1]);
 
 }

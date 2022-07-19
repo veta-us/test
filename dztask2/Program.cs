44173c4c1b499be552bf49aabea4afce3afb24f6
lisa <lisma_li@mail.ru>
2022-07-20 00:33:35 +0300

    сделала задачу 6 из дз

---
+++ dztask2/Program.cs
@@ -0,0 +1,24 @@
+﻿string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+string? inputLineThree = Console.ReadLine();
+
+
+if(inputLineOne != null && inputLineTwo != null & inputLineThree != null)
+{
+    int inputNumberOne = int.Parse(inputLineOne);
+    int inputNumberTwo = int.Parse(inputLineTwo);
+    int inputNumberThree = int.Parse(inputLineThree);
+
+
+    if(inputNumberOne > inputNumberTwo)
+    {
+       if(inputNumberOne > inputNumberThree);
+       Console.WriteLine(inputNumberOne);
+       
+    }
+    else if (inputNumberTwo > inputNumberThree);
+    {
+        Console.WriteLine(inputNumberTwo);
+    }
+    
+}
---
+++ dztask3/Program.cs
@@ -0,0 +1,18 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+
+{
+    int inputNumber = int.Parse(inputLine);
+
+    int outputNumber =  inputNumber%2;
+
+    if( outputNumber == 0)
+    {
+        Console.WriteLine("четное");
+    }
+    else 
+    {
+       Console.WriteLine("нечетное"); 
+    }
+}

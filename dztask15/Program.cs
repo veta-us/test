59c593299c4c1ea378c309ea96c35f6c9d055d36
lisa <lisma_li@mail.ru>
2022-07-31 01:43:51 +0300

    add dz task15

---
+++ dztask15/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число дня недели");
+
+string? inputLine = Console.ReadLine();
+if(inputLine != null)
+
+{
+    int inputNumber = int.Parse(inputLine);
+
+if((inputNumber == 6)||(inputNumber == 7))
+{
+    Console.WriteLine("выходной день недели");
+}
+else
+{
+    Console.WriteLine("рабочий день недели");
+}
+
+}
+
+
+// string[] weekDays = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };

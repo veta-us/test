da0a9afda2bfd6675cc9edfe9877e057f0b6279a
lisa <lisma_li@mail.ru>
2022-07-31 01:31:58 +0300

    add dztask 13

---
+++ dztask13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите трехзначное число");
+
+string? inputLine = Console.ReadLine();
+
+   Char[] myArray = inputLine.ToCharArray();
+
+   if(myArray.Length>=3)
+   {
+    Console.WriteLine("третья цифра :", myArray[2]);
+   }
+   else
+   {
+    Console.WriteLine("третьей цифры нет");
+   }
+
+   Console.WriteLine(myArray[2]);
+
+
+
+
+
+
+

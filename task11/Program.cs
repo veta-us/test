f10b04929e9c30e486742d5b7462bf237b77a183
lisa <lisma_li@mail.ru>
2022-07-31 00:14:50 +0300

    add task 11

---
+++ task11/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100,1000);
+Console.WriteLine("Число:" + number);
+int numbersecondDigit = number/100;
+int numberThirdDigit = number&10;
+Console.WriteLine("Число после удаления второй цифры: " + numbersecondDigit + numberThirdDigit);
+
--- task9/Program.cs
+++ task9/Program.cs
@@ -1,9 +1,10 @@
 ﻿System.Random numberSintezator = new System.Random ();
 
-int number = numberSintezator.Next(10,100);
+
+void variant1(System.Random numberSintezator)
+{
+    int number = numberSintezator.Next(10,100);
  Console.WriteLine(number);
- 
-// Вариант 1
 
 int firstNumber = number/10;
 int secondNumber = number%10;
@@ -11,11 +12,44 @@ int secondNumber = number%10;
 if(firstNumber>secondNumber)
 {
     Console.WriteLine(firstNumber);
-
 }
-
 else
 {
     Console.WriteLine(secondNumber);
 }
+}
 
+void variant2System.Random ()
+{
+    Console.WriteLine("The random number is: ");
+    Console.WriteLine(numberKir);
+
+    string stringNum = numberKir.ToString();
+
+    Console.WriteLine("The biggest digit is : ");
+    if (stringNum[0] > stringNum[1])
+    {
+        Console.WriteLine(stringNum[0]);
+    }
+    else
+    {
+        Console.WriteLine(stringNum[1]);
+    }
+}
+
+
+
+
+// вариант 3 
+char[] digits = numberSintezator.Next(10,100).ToString().ToCharArray();
+ 
+ Console.WriteLine(digits);
+
+firstNumber = ((int)digits[0])-48;
+secondNumber = ((int)digits[1])-48;
+
+int resultNumber = firstNumber>secondNumber?resultNumber=firstNumber:resultNumber=secondNumber;
+
+Console.WriteLine(resultNumber);
+
+// ладно, с методами я разберусь позже. До момента сборки методов, все у меня получалось и было аккуратно. 

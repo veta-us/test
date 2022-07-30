ad3ff79aa90b83bd06dd9b99933d1132e603702e
lisa <lisma_li@mail.ru>
2022-07-30 23:27:24 +0300

    siminar task 9, variant 1

---
+++ task9/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Random numberSintezator = new System.Random ();
+
+int number = numberSintezator.Next(10,100);
+ Console.WriteLine(number);
+ 
+// Вариант 1
+
+int firstNumber = number/10;
+int secondNumber = number%10;
+
+if(firstNumber>secondNumber)
+{
+    Console.WriteLine(firstNumber);
+
+}
+
+else
+{
+    Console.WriteLine(secondNumber);
+}
+

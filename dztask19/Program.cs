43a0f66594b12008b3418786f8bf0266c16386e9
lisa <lisma_li@mail.ru>
2022-08-04 17:30:06 +0300

    datask 19 done

--- dztask19/Program.cs
+++ dztask19/Program.cs
@@ -1,18 +1,16 @@
 ﻿int a = int.Parse(Console.ReadLine());
 
-
 int D1 = a/10000;
 int D2 = (a/1000)%10;
 int D4 = (a/10)%10;
 int D5 = a%10;
 
-if(D1 = D5 && D2 = D4)
+if(D1 == D5 && D2 == D4)
    {
-    Console.WriteLine("полиндром");
+    Console.WriteLine("Да");
    }
    else
    {
-    Console.WriteLine("не полиндром");
+    Console.WriteLine("нет");
    }
 
-  

941669a072cfc3b436d472ff395e0386bac35c62
lisa <lisma_li@mail.ru>
2022-08-06 18:11:58 +0300

    task 23 simple variant

---
+++ task24/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("ВВедите число");
+
+int inputNumber = int.Parse(Console.ReadLine());
+
+void VariantSimple()
+{
+    int sumOfNumber =0;
+    for(int i=1;i<=inputNumber;i++)
+    {
+        sumOfNumber +=i;
+        // sumOfNumber = sumOfNumber +1
+
+    }
+Console.WriteLine("сумма чисел от 1 до" +  inputNumber + "=" + sumOfNumber);
+
+}
+
+ VariantSimple();

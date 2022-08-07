1d4283255d7200bead2f747f29128308ddb63fce
lisa <lisma_li@mail.ru>
2022-08-07 17:55:05 +0300

    dz task 27

---
+++ dztask27/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("ВВедите число ");
+
+int A = int.Parse(Console.ReadLine());
+
+int B = 0;
+
+void Conculated()
+{
+    
+    while(A>0)
+    {
+        B=B+A%10;
+        A=A/10;
+
+    }
+    Console.WriteLine(B);
+
+}
+
+Conculated();
+
+

7d6f03c7aceedbe1b52fe6d8db59374553878920
lisa <lisma_li@mail.ru>
2022-08-07 17:39:26 +0300

    dz task 25

---
+++ dztask25/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("ВВедите числа А и В ");
+
+void SuperProstoy()
+
+{
+int inputNumberA = int.Parse(Console.ReadLine());
+int inputNumberB = int.Parse(Console.ReadLine());
+Console.WriteLine(Math.Pow(inputNumberA, inputNumberB));
+
+}
+
+SuperProstoy();
+
+
+
+
+
+
--- task12/Program.cs
+++ task12/Program.cs
@@ -1,21 +1,21 @@
-﻿// string? inputLineOne = Console.ReadLine();
-// string? inputLineTwo = Console.ReadLine();
+﻿string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
 
-// if(inputLineOne != null && inputLineTwo != null)
-// {
-//     int inputNumberOne = int.Parse(inputLineOne);
-//     int inputNumberTwo = int.Parse(inputLineTwo);
+if(inputLineOne != null && inputLineTwo != null)
+{
+    int inputNumberOne = int.Parse(inputLineOne);
+    int inputNumberTwo = int.Parse(inputLineTwo);
 
-//     int div = inputNumberOne&inputNumberTwo;
+    int div = inputNumberOne&inputNumberTwo;
 
-//     if(div == 0)
-//     {
-//         Console.WriteLine("yes");
-//     }
-//     else
-//     {
-//         Console.WriteLine(div);
-//     }
+    if(div == 0)
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine(div);
+    }
 
-// }
+}
 

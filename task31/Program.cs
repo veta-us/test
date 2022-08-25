8bc0b9fa1c977104e9bf17f6891837f5341c9381
lisa <lisma_li@mail.ru>
2022-08-25 20:02:21 +0300

    add task 31,32

---
+++ task31/Program.cs
@@ -0,0 +1,62 @@
+﻿//Переменные для накопления результата
+int posetivSum = 0;
+int negativSum = 0;
+
+//Метод возвращает массив заполненный случайнми числами от -9 до 9
+int[] FillingArray()
+{
+    
+    int[] outArray = new int[12];
+    //буферная переменная
+    int i = 0;
+    //создаем экземпляр класса рандомайзер
+    System.Random numberSintesator = new System.Random();
+    
+    while(i<12)
+    {
+     //Получаем новое значение
+       outArray[i] = numberSintesator.Next(-9,10);
+       //Увеличиваем инкримент 
+       i++;
+    }
+    //Возвращаем значение
+    return outArray;
+}
+
+//Метод подсчета положительных и отрицательных значений
+void ColculateTask(int[] outArray)
+{
+    int i = 0;
+    while (i<12)
+    {
+        if(outArray[1]>0)
+        {
+           posetivSum++;
+        }
+        else{
+            negativSum++;
+        }
+        i++;
+    }
+
+
+}
+
+//метод принта результатов
+void Print()
+{
+    Console.WriteLine(posetivSum);
+    Console.WriteLine(negativSum);
+}
+
+void VariantNaive()
+{
+    //вызов методов
+int[] befferArrray = FillingArray();
+ColculateTask(befferArrray);
+Print();
+}
+
+VariantNaive();
+
+
---
+++ task32/Program.cs
@@ -0,0 +1,56 @@
+﻿// метод возвращает массив заполненный случайными числами от -9 до 9
+int[] FillingArray()
+{
+    
+    int[] outArray = new int[12];
+    //буферная переменная
+    int i = 0;
+    //создаем экземпляр класса рандомайзер
+    System.Random numberSintesator = new System.Random();
+    
+    while(i<12)
+    {
+     //Получаем новое значение
+       outArray[i] = numberSintesator.Next(-9,10);
+       //Увеличиваем инкримент 
+       i++;
+    }
+    //Возвращаем значение
+    return outArray;
+}
+
+//Метод печатает массив
+void PrintIntArray(int[] inputArray)
+{
+    //Буыерная переменная
+    int i = 0;
+    //Пробегаем все элементы массива
+    while (i < inputArray.Length -1)
+    {
+        //Выводим элемент массива
+        Console.Write(inputArray[i]+ ", ");
+       //Увеличиваем инкримент
+        i++;
+    }
+    //Выводим эллемент массива
+    Console.WriteLine(inputArray[i]);
+}
+
+//Метод инвертирует элементы массива (простой)
+int[] ColculateTask(int[] inputArray)
+{
+    int[] outputArray = new int[12];
+    int i = 0;
+    while (i<12)
+    {
+        outputArray[i]=outputArray[i]*(-1);
+   
+        i++;
+    }
+    return outputArray;
+}
+
+int[] bufferArray = FillingArray();
+PrintIntArray(bufferArray);
+int[] resultArray = ColculateTask(bufferArray);
+PrintIntArray(resultArray);

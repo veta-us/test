250e7e2ba2aa13e2e3f8cf44048b5b037ce3f9d8
lisa <lisma_li@mail.ru>
2022-09-07 20:52:32 +0300

    add tz task 60

--- dztask60/Program.cs
+++ dztask60/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿int[,,] FillTwoDimArray(int A, int B, int C)
+{
+      System.Random numberSyntezator = new System.Random();
+      int i = 0;
+      int j = 0;
+      int k =0;
+      int[,,] outArray = new int[A, B, C];
+      while (i < A)
+      {
+            j = 0;
+            while (j < B)
+            {
+                 while (k < C)
+                 {
+                  outArray[i, j, k] = numberSyntezator.Next(0, 101);
+                  k++;
+                 }
+                  j++;
+            }
+            i++;
+      } 
+      return outArray;
+}
+
+
+
+int[,,] array3D = FillTwoDimArray(2, 2, 2);
+Console.WriteLine(array3D);
